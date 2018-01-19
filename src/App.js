76b80af3f842c39ca5ba4cb05df0102a8ab5fdad
Paweł Adam Wojciechowski <pawel.wojciechowski93@gmail.com>
2018-01-19 11:53:18 +0100

    view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className = "header">
+            <img src= "/logo01.jpg"/>
+            <h1 className = "title">Zandaka</h1>
+            <h2 className = "secondTitle">Without fight we do not survive</h2>
+        </div>
+
+        <div className = "middle">
+
+          <div className = "sides">
+            <img src = "/left01.jpg" />
+          </div>
+
+          <div className = "bord"></div>
+
+          <div className = "sides">
+            <img src = "/right01.jpg" />
+          </div>
+
+        </div>
+
+        <div className = "footer"></div>
       </div>
     );
   }
