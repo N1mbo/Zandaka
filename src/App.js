f0c87e06b6bc75c3eadbb110f0e2091350fd95de
Paweł Adam Wojciechowski <pawel.wojciechowski93@gmail.com>
2018-01-19 12:58:35 +0100

    them in index

--- src/App.js
+++ src/App.js
@@ -4,29 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className = "header">
-            <img src= "/logo01.jpg"/>
-            <h1 className = "title">Zandaka</h1>
-            <h2 className = "secondTitle">Without fight we do not survive</h2>
-        </div>
-
-        <div className = "middle">
-
-          <div className = "sides">
-            <img src = "/left01.jpg" />
-          </div>
-
-          <div className = "bord"></div>
-
-          <div className = "sides">
-            <img src = "/right01.jpg" />
-          </div>
-
-        </div>
-
-        <div className = "footer"></div>
-      </div>
+      <canvas></canvas>
     );
   }
 }
